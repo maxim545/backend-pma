ef50524ec35dd90d60c6eeeb20aee1c3a8959e29
maxim545 <maxximc95@gmail.com>
2022-11-11 00:40:03 +0300

    add

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/${applicationName}');
+    await mongoose.connect('mongodb+srv://admin:admin123456789@backendrss.bgrvvy7.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
